598fa22a2f5d96b0e1eeddab55d6397893176db4
Thais Ribeiro <thaisribeiro@softbox.com.br>
2022-08-05 16:18:48 -0300

    chore: config

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app = FastAPI(title='Sumarizador')
 app.include_router(api_router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1:8000', port='8000', reload=True)
+    uvicorn.run(app)
