36201f4f05fdde22456f76c2077563ed5ec078b1
Thais Ribeiro <thaisribeiro@softbox.com.br>
2022-08-05 16:31:11 -0300

    translate

--- src/api/routers/__init__.py
+++ src/api/routers/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.routers import summarization
+from src.api.routers import summarization
 
 api_router = APIRouter()
 api_router.include_router(summarization.router)
